666a83e3d94331f083f872e9da1e33dc77f620f5
Mark Ting <weite.mting@gmail.com>
2022-11-26 19:47:00 -0500

    Update sim_test example

--- sim_test.py
+++ sim_test.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     s1 = simulation.create_server()
     simulation.online_client(c1)
     simulation.online_client(c2)
-    simulation.assign_client_to_server(s1, c2)
+    simulation.assign_client_to_server(client_id=c2, server_id=s1)
     simulation.print_actors()
 
     simulation.run()
