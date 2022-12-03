866754a65af3841c2b4351f74cdc676d6d91121f
Luke Bailey <lukebailey@college.harvard.edu>
2022-12-03 17:48:33 -0500

    Allow pretraining

--- training_latency_eval/resnet_latency_eval.py
+++ training_latency_eval/resnet_latency_eval.py
@@ -172,7 +172,7 @@ def get_cfar_dataset(
 
 def get_resnet_and_optimizer(classes):
 
-    model_ft = models.resnet18(pretrained=False)
+    model_ft = models.resnet18(pretrained=True)
     num_ftrs = model_ft.fc.in_features
     # Here the size of each output sample is set to 2.
     # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
